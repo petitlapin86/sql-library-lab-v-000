8434253cfc62af5876827c2aeb81068c3a23b265
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:26:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,11 +10,11 @@ INSERT INTO books (id, title, year, series_id) VALUES
 (6, "Moonboots Gets Lost Under the Bed", 2012, 2);
 
 INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES
-(1, "Moonboots", "rabbit", "im so cute", 1),
-(2, "Mable", "rabbit", "im so fancy", 1),
-(3, "Curtain","object", "i have no feelings", 1),
-(4, "Slipper", "object", "i have no thoughts", 1),
-(5, "Amos", "human", "im super cool", 2),
-(6, "Elliot", "human", "im actually cooler", 2),
-(7, "Dan", "human", "nah nah nah", 2),
-(8, "Paige", "human", "ooh shiny", 2);
+(1, "Moonboots", "rabbit", "im so cute", 1, 1),
+(2, "Mable", "rabbit", "im so fancy", 1, 1),
+(3, "Curtain","object", "i have no feelings", 1, 1),
+(4, "Slipper", "object", "i have no thoughts", 1, 1),
+(5, "Amos", "human", "im super cool", 2, 2),
+(6, "Elliot", "human", "im actually cooler", 2, 2),
+(7, "Dan", "human", "nah nah nah", 2, 2),
+(8, "Paige", "human", "ooh shiny", 2, 2);
