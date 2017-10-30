9f14384aa7cefce440a19cc6fc5ba02fde3e972c
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:47:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,10 @@ end
 
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto 
+    FROM characters 
+    ORDER BY LENGTH(motto) DESC 
+    LIMIT 1"
 end
 
 
