2f3b205aede19ecadef7f2dc2204e89107740e99
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:50:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,10 +12,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT (*)
+  "SELECT species, COUNT(*)
   FROM characters
   GROUP BY species
-  ORDER BY COUNT(species) "
+  ORDER BY COUNT(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
