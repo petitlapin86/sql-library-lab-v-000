ee2fd8f686f33a0d1375f3ec94f4f60d3576227c
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:46:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books series_id = 1 GROUP BY books.year";
+  "SELECT books.title, books.year FROM books WHERE series_id = 1 GROUP BY books.year";
 end
 
 
