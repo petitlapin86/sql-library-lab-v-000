59206972ea7e0451e4f971ab72a58b68cf5ff193
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:51:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,8 +13,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(*)
-  FROM characters
-  GROUP BY species
+  FROM characters,
+  GROUP BY species,
   ORDER BY COUNT(species)"
 end
 
