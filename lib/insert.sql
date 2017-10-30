7d42c81da909238fa795b2c114d9389a1dd502f9
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:17:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,6 +5,6 @@ INSERT INTO books (id, title, year, series_id) VALUES
 (1, "Moonboots finds a slipper", 2017, 1),
 (2, "Moonboots and the curtain", 2017, 1),
 (3, "Rabbit Hole", 2016, 1),
-(4, "The Tale of Two Tails", 2015, 2), 
+(4, "The Tale of Two Tails", 2015, 2),
 (5, "Adventures of Fudderbun", 2014, 2),
-(6, "Moonboots Gets Lost Under the Bed", 2012, 2) ;
+(6, "Moonboots Gets Lost Under the Bed", 2012, 2);
