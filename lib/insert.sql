1f47b31a03eccae65f0aabd299abc2581afeaf24
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:25:41 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,13 @@ INSERT INTO books (id, title, year, series_id) VALUES
 (4, "The Tale of Two Tails", 2015, 2),
 (5, "Adventures of Fudderbun", 2014, 2),
 (6, "Moonboots Gets Lost Under the Bed", 2012, 2);
+
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES
+(1, "Moonboots", "rabbit", "im so cute", 1),
+(2, "Mable", "rabbit", "im so fancy", 1),
+(3, "Curtain","object", "i have no feelings", 1),
+(4, "Slipper", "object", "i have no thoughts", 1),
+(5, "Amos", "human", "im super cool", 2),
+(6, "Elliot", "human", "im actually cooler", 2),
+(7, "Dan", "human", "nah nah nah", 2),
+(8, "Paige", "human", "ooh shiny", 2);
