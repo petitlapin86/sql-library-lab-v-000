8f577afb767ec83e41ce4d78bd165e7c36b4b855
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:17:31 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,9 +2,9 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 (1, "Mable The Magnificent", 1, 1), (2, "Mable The Brave", 2, 2);
 
 INSERT INTO books (id, title, year, series_id) VALUES
-(1, "Mable The Magnificent", 2017, 1), 
-(2, "Mable The Brave", 2017, 1), 
-(3, "Mable The Brave", 2016, 1), 
-(4, "Mable The Brave", 2015, 2), 
-(5, "Mable The Brave", 2014, 2),
-(6, "Mable The Brave", 2012, 2) ;
+(1, "Moonboots finds a slipper", 2017, 1),
+(2, "Moonboots and the curtain", 2017, 1),
+(3, "Rabbit Hole", 2016, 1),
+(4, "The Tale of Two Tails", 2015, 2), 
+(5, "Adventures of Fudderbun", 2014, 2),
+(6, "Moonboots Gets Lost Under the Bed", 2012, 2) ;
