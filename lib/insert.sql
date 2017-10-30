32dbe11b776926e7a26a3154dd4c5688e82f6eb7
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-30 20:15:30 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,10 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 (1, "Mable The Magnificent", 1, 1), (2, "Mable The Brave", 2, 2);
+
+INSERT INTO books (id, title, year, series_id) VALUES
+(1, "Mable The Magnificent", 2017, 1), 
+(2, "Mable The Brave", 2017, 1), 
+(3, "Mable The Brave", 2016, 1), 
+(4, "Mable The Brave", 2015, 2), 
+(5, "Mable The Brave", 2014, 2),
+(6, "Mable The Brave", 2012, 2) ;
